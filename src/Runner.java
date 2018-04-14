7ff561d65095801af4b08015f3fa4b4483ae8d20
GreegAV <greegav@gmail.com>
2018-04-14 22:50:29 +0300

    Добавил выражения вида A*B +/- C*D и A*B +/-C +/- D

--- src/Runner.java
+++ src/Runner.java
@@ -3,48 +3,51 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Runner {
-    static String resTask;
-    static int resultOfTask;
-    static long iter = 0;
+    private static String resTask;
+    private static int resultOfTask;
 
     public static void main(String[] args) {
         int numTasks = 0;
-        int totalRight = 0;
-        int totalWrong = 0;
-
+        int totalRight;
+        int totalWrong;
 
         Scanner scanner = new Scanner(System.in);
 
-        try (
-                FileWriter fileWriter = new FileWriter("results.txt", true);
-//                FileWriter fileTracker = new FileWriter("track.txt", true)
-        ) {
-            // запись всей строки
+        try (FileWriter fileWriter = new FileWriter("results.txt", true)) {
             do {
                 System.out.println("Введите количество примеров (1-12).");
                 System.out.print("Для выхода ввести 0. ");
-                numTasks = Integer.parseInt(scanner.nextLine());
+                try {
+                    numTasks = Integer.parseInt(scanner.nextLine());
+                }
+                catch (Exception ex){
+                    System.out.println("Допустим ввод только цифр! Запустите программу еще раз.");
+                    continue;
+                }
                 if ((numTasks < 1) || (numTasks > 12)) continue;
 
                 fileWriter.write("Блок из " + numTasks + " примеров.\n\r");
-                // random1 - первое слагаемое(целое до 100)
-                // random2 - первый оператор(=/-)
-                // random3 - первый множитель(целое до 10)
-                // random4 - второй множитель(целое до 10)
-                // random5 - второй оператор(=/-)
-                // random6 - последнее слагаемое(целое до 100)
-                totalRight=0;
-                totalWrong=0;
+                totalRight = 0;
+                totalWrong = 0;
                 for (int i = 1; i <= numTasks; i++) {
-//                    generateValidExpression(fileTracker);
-                    generateValidExpression();
+                    switch ((int) (Math.random() * 10 % 3)) {
+                        case 1: {
+                            generateValidExpression();
+                            break;
+                        }
+                        case 2: {
+                            generateValidExpression2();
+                            break;
+                        }
+                        default: {
+                            generateValidExpression3();
+                            break;
+                        }
+                    }
 
-                    System.out.print("Пример " + i + ": " + resTask+"\t"+"Ответ : ");
-                    fileWriter.write("Пример " + i + ": " + resTask + "\t"+"Ответ : ");
-//                    System.out.print("Ответ : ");
-//                    fileWriter.write("Ответ ");
+                    System.out.print("Пример " + i + ": " + resTask + "\t" + "Ответ : ");
+                    fileWriter.write("Пример " + i + ": " + resTask + "\t" + "Ответ : ");
                     int answer = Integer.parseInt(scanner.nextLine());
-
                     if (answer == resultOfTask) {
                         System.out.println("Правильно!");
                         fileWriter.write("" + answer + " правильный.\n\r");
@@ -59,69 +62,123 @@ public class Runner {
                 System.out.println(results);
                 fileWriter.write(results);
                 fileWriter.flush();
-
             } while (numTasks > 0);
-
-        } catch (IOException ex) {
+        } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
-
         System.out.println("Всего хорошего!");
     }
 
-    //    private static void generateValidExpression(FileWriter fileTracker) throws IOException {
     private static void generateValidExpression() {
-        boolean valid = true;
+        // A +/- B*C +/- D
+        boolean valid;
         do {
             valid = true;
             resTask = "";
             resultOfTask = 0;
-//            fileTracker.write("\n\r");
 
-            int random2 = (int) (Math.random() * 10 % 2);
-            int random5 = (int) (Math.random() * 10 % 2);
-            int random3 = (int) (Math.random() * 10);
-            int random4 = (int) (Math.random() * 10);
-            int random1 = (random2 == 1) ? (int) (Math.random() * 100) : (int) (50 + Math.random() * random3*random4);
-            int random6 = (int) (Math.abs(Math.random() * (random1 - random3 * random4)));
-            iter++;
-//            fileTracker.write("Iteration " + iter + " Source: " + random1 + ((random2==1)?"+":"-")+ random3+ "*" + random4 + ((random5==1)?"+":"-") +  random6 + "\t");
-//            fileTracker.flush();
-            if (random2 == 1) {
-                resultOfTask = random1 + random3 * random4;
-                resTask += random1 + "+" + random3 + "*" + random4;
-//                valid = true;
+            int randSign1 = (int) (Math.random() * 10 % 2);
+            int randSign2 = (int) (Math.random() * 10 % 2);
+            int randomB = (int) (Math.random() * 10);
+            int randomC = (int) (Math.random() * 10);
+            int randomA = (int) (Math.random() * 100);
+            int randomD = (int) (Math.random() * 100);
+//            int randomA = (randSign1 == 1) ? (int) (Math.random() * 100) : (int) (50 + Math.random() * randomB * randomC);
+//            int randomD = (int) (Math.abs(Math.random() * (randomA - randomB * randomC)));
+            if (randSign1 == 1) {
+                resultOfTask = randomA + randomB * randomC;
+                resTask += randomA + "+" + randomB + "*" + randomC;
             } else {
-                if (random1 >= random3 * random4) {
-                    resultOfTask = random1 - random3 * random4;
-                    resTask += random1 + "-" + random3 + "*" + random4;
-//                    valid = true;
+                if (randomA >= randomB * randomC) {
+                    resultOfTask = randomA - randomB * randomC;
+                    resTask += randomA + "-" + randomB + "*" + randomC;
                 } else {
-                    resultOfTask = random1 - random3 * random4;
-                    resTask += random1 + "-" + random3 + "*" + random4;
-//                    fileTracker.write("Условие: " + resTask + " Ответ: " + resultOfTask + "\n\r");
                     valid = false;
-//                    fileTracker.flush();
                     continue;
                 }
             }
 
-            if (random5 == 1) {
-                resultOfTask = resultOfTask + random6;
-                resTask += "+" + random6;
-//                valid = true;
+            if (randSign2 == 1) {
+                resultOfTask = resultOfTask + randomD;
+                resTask += "+" + randomD;
             } else {
-                if (resultOfTask >= random6) {
-                    resultOfTask = resultOfTask - random6;
-                    resTask += "-" + random6;
-//                    valid = true;
+                if (resultOfTask >= randomD) {
+                    resultOfTask = resultOfTask - randomD;
+                    resTask += "-" + randomD;
                 } else {
-                    resultOfTask = resultOfTask - random6;
-                    resTask += "-" + random6;
-//                    fileTracker.write("Условие: " + resTask + "Ответ: " + resultOfTask + "\n\r");
                     valid = false;
-//                    fileTracker.flush();
+//                    continue;
+                }
+            }
+        } while (!valid);
+    }
+
+    private static void generateValidExpression2() {
+        // A*B +/- C*D
+        boolean valid;
+        do {
+            valid = true;
+            resTask = "";
+            resultOfTask = 0;
+
+            int randSign1 = (int) (Math.random() * 10 % 2);
+            int randomA = (int) (Math.random() * 10);
+            int randomB = (int) (Math.random() * 10);
+            int randomC = (int) (Math.random() * 10);
+            int randomD = (int) (Math.random() * 10);
+            if (randSign1 == 1) {
+                resultOfTask = randomA * randomB + randomC * randomD;
+                resTask += randomA + "*" + randomB + "+" + randomC + "*" + randomD;
+            } else {
+                if (randomA * randomB > randomC * randomD) {
+                    resultOfTask = randomA * randomB - randomC * randomD;
+                    resTask += randomA + "*" + randomB + "-" + randomC + "*" + randomD;
+                } else {
+                    valid = false;
+//                    continue;
+                }
+            }
+        } while (!valid);
+    }
+
+    private static void generateValidExpression3() {
+        // A*B +/- C +/- D
+        boolean valid;
+        do {
+            valid = true;
+            resTask = "";
+            resultOfTask = 0;
+
+            int randSign1 = (int) (Math.random() * 10 % 2);
+            int randSign2 = (int) (Math.random() * 10 % 2);
+            int randomA = (int) (Math.random() * 10);
+            int randomB = (int) (Math.random() * 10);
+            int randomC = (int) (Math.random() * 100);
+            int randomD = (int) (Math.random() * 100);
+
+            if (randSign1 == 1) {
+                resultOfTask = randomA * randomB + randomC;
+                resTask += randomA + "*" + randomB + "+" + randomC;
+            } else {
+                if (randomA * randomB >= randomC) {
+                    resultOfTask = randomA * randomB - randomC;
+                    resTask += randomA + "*" + randomB + "-" + randomC;
+                } else {
+                    valid = false;
                     continue;
+                }
+            }
+
+            if (randSign2 == 1) {
+                resultOfTask += randomD;
+                resTask += "+" + randomD;
+            } else {
+                if (resultOfTask >= randomD) {
+                    resultOfTask -= randomD;
+                    resTask += "-" + randomD;
+                } else {
+                    valid = false;
+//                    continue;
                 }
             }
         } while (!valid);
@@ -136,7 +193,6 @@ public class Runner {
         res += totalWrong;
         res += "\n\r";
         int resultTotal = (int) Math.ceil((12 * totalRight / numTasks));
-//        int resultTotal=totalRight;
         res += "Оценка: ";
         res += resultTotal;
         res += "\n\r";
